155a961d8e26f4abe65bb722712a8667ae0796d1
Sharun <sharunksplus@gmail.com>
2021-05-14 23:44:04 +0530

    enabled bot polling and sending bot id when added to the group

--- src/telegram.js
+++ src/telegram.js
@@ -4,7 +4,7 @@ const Configuration = require('./configuration')
 class Telegram {
     constructor() {
         this.config = new Configuration('config.json')
-        this.bot = new bot(this.config.readConfig().telegram_bot_token)
+        this.bot = new bot(this.config.readConfig().telegram_bot_token, { polling: true })
         this.sendText = (text) => {
             try {
                 this.bot.sendMessage(this.config.readConfig().telegram_group_id, text, {
@@ -16,7 +16,14 @@ class Telegram {
             }
             
         } 
+        this.bot.on("new_chat_members", async (message, metadata) => {
+            if ((await this.bot.getMe()).id == message['new_chat_participant']['id']) {
+                this.bot.sendMessage(message.chat.id, `The chat id is \`${message.chat.id}\``, {
+                    "parse_mode": "Markdown"
+                })
+            }
+        })
     }
 }
 
-module.exports = new Telegram()
+module.exports = new Telegram() 
