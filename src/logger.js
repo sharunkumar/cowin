4f8e09dc8e7fa6f05104bfd753a9e49ff3cfb6ea
Sharun <sharunksplus@gmail.com>
2021-05-18 22:01:31 +0530

    logger: removed string interpolation

--- src/logger.js
+++ src/logger.js
@@ -3,9 +3,9 @@ const date = require("date-and-time")
 class Logger {
     constructor() {
         const date_format = "YYYY MMM DD hh:mm:ss:SSS A"
-        this.info = object => console.info(`${date.format(new Date(), date_format)}`, object)
-        this.warn = object => console.warn(`${date.format(new Date(), date_format)}`, object)
-        this.debug = object => console.debug(`${date.format(new Date(), date_format)}`, object)
+        this.info = object => console.info(date.format(new Date(), date_format), object)
+        this.warn = object => console.warn(date.format(new Date(), date_format), object)
+        this.debug = object => console.debug(date.format(new Date(), date_format), object)
     }
 }
 
